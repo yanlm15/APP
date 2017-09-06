d00ac46c3545aefa9e7b28321de590634ac73a74
极速客 <1330528508@qq.com>
2017-09-06 19:19:35 +0800

    提供了搜索结果的界面

---
+++ MakeBigNews/app/src/main/java/com/bignewsmaker/makebignews/FunctionActivity/SearchResultActivity.java
@@ -0,0 +1,28 @@
+package com.bignewsmaker.makebignews.FunctionActivity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bignewsmaker.makebignews.ConstData;
+import com.bignewsmaker.makebignews.R;
+import com.bignewsmaker.makebignews.Speaker;
+
+/**
+ * Created by liminyan on 06/09/2017.
+ * 这个界面我们讨论一下再决定要不要
+ */
+
+public class SearchResultActivity extends AppCompatActivity {
+
+    private ConstData const_data = ConstData.getInstance();// 设置访问全局变量接口
+    private Speaker speaker = Speaker.getInstance();// 设置语音系统接口
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //设置输入监控
+        //设置更新函数
+
+    }
+}
