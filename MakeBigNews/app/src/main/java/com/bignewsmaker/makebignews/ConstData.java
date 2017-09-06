a15da28d33c95d0ff9a52f42d0ce328180f3fd14
极速客 <1330528508@qq.com>
2017-09-06 19:19:08 +0800

    更新ConstData

--- MakeBigNews/app/src/main/java/com/bignewsmaker/makebignews/ConstData.java
+++ MakeBigNews/app/src/main/java/com/bignewsmaker/makebignews/ConstData.java
@@ -14,7 +14,9 @@ public class ConstData {
     private double ligth_rate = 1; //屏幕亮度比例
     private boolean show_picture = true; //图片显示标签
 
-    private News cur_news = null ;
+    private News cur_news = null ;// 当前新闻列表
+    private LIST cur_list = null ;// 当前选中新闻
+    private News search_result = null ;// 当前的搜索结果
     private static ConstData cur;
 
     public void init()
@@ -23,21 +25,33 @@ public class ConstData {
         boolean show_picture = true;
     }
 
+    public void setSearch_result(News search_result) {
+        this.search_result = search_result;
+    }//初始化搜索结果
+
     public void setCur_news(News cur_news) {
         this.cur_news = cur_news;
-    }
+    }//初始新闻列表
 
-    public News getCur_news() {
-        return cur_news;
-    }
+    public void setCur_list(LIST cur_list) {
+        this.cur_list = cur_list;
+    }//初始化选中新闻
 
     public void setLigth_rate(double ligth_rate) {
         this.ligth_rate = ligth_rate;
-    }
+    }//初始化亮度参数
 
     public void setShow_picture(boolean show_picture) {
         this.show_picture = show_picture;
+    }//初始化图片显示标签
+
+    public News getSearch_result() {
+        return search_result;
     }
+
+    public News getCur_news() {return cur_news;}
+
+    public LIST getCur_list() {return cur_list;}
 
     public double getLigth_rate() {
         return ligth_rate;
