0eca9900abe9ce31348df648019e95c66fd03e50
极速客 <1330528508@qq.com>
2017-09-06 15:10:15 +0800

    提供搜索界面以及设置界面

---
+++ MakeBigNews/app/src/main/java/com/bignewsmaker/makebignews/FunctionActivity/SearchActivity.java
@@ -0,0 +1,26 @@
+package com.bignewsmaker.makebignews.FunctionActivity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.bignewsmaker.makebignews.NewsAdapter;
+import com.bignewsmaker.makebignews.R;
+
+
+/**
+ * Created by liminyan on 06/09/2017.
+ * 搜索界面
+ */
+
+public class SearchActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //设置输入监控
+        //设置更新函数
+
+    }
+}
---
+++ MakeBigNews/app/src/main/java/com/bignewsmaker/makebignews/FunctionActivity/SetActivity.java
@@ -0,0 +1,21 @@
+package com.bignewsmaker.makebignews.FunctionActivity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bignewsmaker.makebignews.R;
+
+/**
+ * Created by liminyan on 06/09/2017.
+ */
+
+public class SetActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //设置输入监控
+        //设置更新函数
+
+    }
+}
