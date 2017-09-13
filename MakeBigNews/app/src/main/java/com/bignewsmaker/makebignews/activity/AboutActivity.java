44b092825978a735425469fd2e4de3827b6604b0
极速客 <1330528508@qq.com>
2017-09-13 12:37:19 +0800

    Revert "Merge branch 'master' of https://github.com/yanlm15/APP"
    This reverts commit edd3648c8ce95472ff39c12445f0342ef92024e6, reversing
    changes made to 6bf6336e1ff94b999d7e30be78d0c3278d86a516.

--- MakeBigNews/app/src/main/java/com/bignewsmaker/makebignews/activity/AboutActivity.java
+++ MakeBigNews/app/src/main/java/com/bignewsmaker/makebignews/activity/AboutActivity.java
@@ -7,7 +7,7 @@ import android.widget.TextView;
 import com.bignewsmaker.makebignews.R;
 import com.bignewsmaker.makebignews.basic_class.ConstData;
 
-import java.util.Set;
+import java.util.HashSet;
 
 public class AboutActivity extends AppCompatActivity {
     private ConstData const_data = ConstData.getInstance();// 设置访问全局变量接口
@@ -16,7 +16,7 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         TextView textView=(TextView)findViewById(R.id.about);
-        Set<String> hs= const_data.getHaveRead();
+        HashSet<String> hs= const_data.getHaveRead();
         String s="";
         for(String e:hs)
             s+=e+"  ";
