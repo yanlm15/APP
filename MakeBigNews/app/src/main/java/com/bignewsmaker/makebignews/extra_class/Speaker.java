77b31b2f6539938aec6ddfe37fe18d59ee678088
极速客 <1330528508@qq.com>
2017-09-13 19:38:40 +0800

    Revert "语音优雅版上线"
    This reverts commit fbbe7b77b93b9c3757cce63f4acbfedb56b223f3.

--- MakeBigNews/app/src/main/java/com/bignewsmaker/makebignews/extra_class/Speaker.java
+++ MakeBigNews/app/src/main/java/com/bignewsmaker/makebignews/extra_class/Speaker.java
@@ -42,7 +42,7 @@ public class Speaker {
 
     private SpeechSynthesizer mTts ;
     //set
-    private String voicer = "xiaofen";
+    private String voicer = "xiaofeng";
     private String mEngineType = SpeechConstant.TYPE_CLOUD;
     private SharedPreferences mSharedPreferences;
     private SynthesizerListener mTtsListener = new SynthesizerListener() {
@@ -114,12 +114,27 @@ public class Speaker {
             mTts.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_CLOUD);
             // 设置在线合成发音人
             mTts.setParameter(SpeechConstant.VOICE_NAME, "xiaofeng");
+            //设置合成语速
+            mTts.setParameter(SpeechConstant.SPEED, mSharedPreferences.getString("speed_preference", "50"));
+            //设置合成音调
+            mTts.setParameter(SpeechConstant.PITCH, mSharedPreferences.getString("pitch_preference", "50"));
+            //设置合成音量
+            mTts.setParameter(SpeechConstant.VOLUME, mSharedPreferences.getString("volume_preference", "50"));
         }else {
             mTts.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_LOCAL);
             // 设置本地合成发音人 voicer为空，默认通过语记界面指定发音人。
-            mTts.setParameter(SpeechConstant.VOICE_NAME, "");
+            mTts.setParameter(SpeechConstant.VOICE_NAME, "xiaofeng");
 
         }
+        //设置播放器音频流类型
+        mTts.setParameter(SpeechConstant.STREAM_TYPE, mSharedPreferences.getString("stream_preference", "3"));
+        // 设置播放合成音频打断音乐播放，默认为true
+        mTts.setParameter(SpeechConstant.KEY_REQUEST_FOCUS, "true");
+
+        // 设置音频保存路径，保存音频格式支持pcm、wav，设置路径为sd卡请注意WRITE_EXTERNAL_STORAGE权限
+        // 注：AUDIO_FORMAT参数语记需要更新版本才能生效
+//        mTts.setParameter(SpeechConstant.AUDIO_FORMAT, "wav");
+//        mTts.setParameter(SpeechConstant.TTS_AUDIO_PATH, Environment.getExternalStorageDirectory()+"/msc/tts.wav");
     }
 
     public void setText(String text) {
@@ -134,6 +149,7 @@ public class Speaker {
     {
 
         String _text = this.text;
+//        mTts = SpeechSynthesizer.createSynthesizer(cur, null);
         mTts = SpeechSynthesizer.createSynthesizer(cur.getApplicationContext(), null);
 
 
@@ -141,12 +157,35 @@ public class Speaker {
         {
             System.out.println("wo cao ni ma");
         }
-        setParam();
+//        setParam();
         int code = mTts.startSpeaking(text, mTtsListener);
 
+
+//        mTextToSpeech=new TextToSpeech(cur, new TextToSpeech.OnInitListener() {
+//
+//            @Override
+//            public void onInit(int status) {
+//                if (status==TextToSpeech.SUCCESS) {
+//                    //设置朗读语言
+//                    int supported=mTextToSpeech.setLanguage(Locale.CHINESE);
+//
+//                    if ((supported!=TextToSpeech.LANG_AVAILABLE)&&(supported!= TextToSpeech.LANG_COUNTRY_AVAILABLE)) {
+//                        Toast.makeText(cur, "不支持当前语言！", 1).show();
+//                    }
+//
+//                    mTextToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+//                }
+//
+//            }
+//
+//        });
     }
 
     public void stop() {
+//        if (mTextToSpeech != null) {
+//            mTextToSpeech.stop();
+//            mTextToSpeech.shutdown();
+//        }
         if( null != mTts ){
             mTts.stopSpeaking();
             // 退出时释放连接
